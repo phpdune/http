3ea8ef4d040587608ce61a1b896342d4f1eeb3ca
coderswat <coderswat@gmail.com>
2023-06-11 16:46:37 +0530

    updated namespace

--- Middlewares/SecureRequest.php
+++ Middlewares/SecureRequest.php
@@ -15,14 +15,14 @@ namespace Dune\Http\Middlewares;
 
 use Closure;
 use Dune\Http\Request;
-use Dune\App;
+use Dune\Core\App;
 use Dune\Http\Middleware\MiddlewareInterface;
 use Dune\Http\Middlewares\Exception\RequestNotSecure;
 
 class SecureRequest implements MiddlewareInterface
 {
     /**
-     * \Dune\App instance
+     * \Dune\Core\App instance
      * 
      * @var App
      */
